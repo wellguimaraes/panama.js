9bb5085a29dece0ed866496bf332a2c87a90a69c
Well Guimaraes <wellguimaraes@gmail.com>
2016-04-04 20:51:47 +0000

    added create store feature

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# riotto
-An event bus for Riot
+# Riotto
+A Flux helper for Riot, React or any other UI library.
+
+`npm install --save-dev riotto`
+
+## Usage
+
+```js
+var riotto = require('riotto');
+
+var store = riotto
+    .createStore()
+    .setState({
+        // app initial state
+    })
+    .couple(function() {
+        // event handlers
+        
+        this.on('something', function(payload) {
+            this.setState({
+                // some state changes
+            });
+        });
+        
+        this.on('somethingElse', function() {
+            this.setState({
+                // some state changes
+            });
+        });
+    })
+    .couple(require('./anotherHandlerSet'));
+    
+    // Then you can trigger events
+    store.trigger('something', payload);
+
+    // And listen to state changes
+    store.on('update', function() {
+        // Update your views?
+    });
+
+```
--- index.js
+++ index.js
@@ -2,15 +2,42 @@ var observable = require('riot-observable');
 
 module.exports = (function () {
     var riotto = {};
+    
+    riotto.createStore = function () {
+      observable(this);
+    
+      var state = {};
+    
+      this.couple = function (anotherStore) {
+        anotherStore.apply(this);
+        return this;
+      }.bind(this);
+    
+      this.getState = function() {
+          return state;
+      };
+    
+      this.setState = function (changes) {
+        Object.assign(state, changes);
+        this.trigger('update');
+      }.bind(this);
+    
+      return this;
+    
+    }.bind({});
 
     riotto.createActions = function (namespace, actionSet) {
         for (var action in actionSet) {
             if (actionSet.hasOwnProperty(action))
                 actionSet[action] = namespace + ':' + action;
         }
+        
         return actionSet;
     };
 
+    /**
+     * @deprecated
+     */
     riotto.createEventBus = function () {
         return observable({});
     };
