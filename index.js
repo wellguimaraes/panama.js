5a0cd54ce3e3b91ef9e9ef3c799b7fe4f9b2dcba
Well Guimaraes <wellguimaraes@gmail.com>
2016-04-04 21:23:43 +0000

    simplifying  createStore

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
 var observable = require('riot-observable');
 
-module.exports = (function () {
+module.exports = (function() {
     var riotto = {};
-    
-    riotto.createStore = function () {
-      observable(this);
-    
-      var state = {};
-    
-      this.couple = function (anotherStore) {
-        anotherStore.apply(this);
-        return this;
-      }.bind(this);
-    
-      this.getState = function() {
-          return state;
-      };
-    
-      this.setState = function (changes) {
-        Object.assign(state, changes);
-        this.trigger('update');
-      }.bind(this);
-    
-      return this;
-    
-    }.bind({});
 
-    riotto.createActions = function (namespace, actionSet) {
+    riotto.createStore = function() {
+        var store = {};
+        var state = {};
+        
+        observable(store);
+
+        this.couple = function(anotherStore) {
+            anotherStore.apply(this);
+            return this;
+        }.bind(store);
+
+        this.getState = function() {
+            return state;
+        };
+
+        this.setState = function(changes) {
+            Object.assign(state, changes);
+            this.trigger('update');
+        }.bind(store);
+
+        return store;
+
+    };
+
+    riotto.createActions = function(namespace, actionSet) {
         for (var action in actionSet) {
             if (actionSet.hasOwnProperty(action))
                 actionSet[action] = namespace + ':' + action;
         }
-        
+
         return actionSet;
     };
 
     /**
      * @deprecated
      */
-    riotto.createEventBus = function () {
+    riotto.createEventBus = function() {
         return observable({});
     };
 
